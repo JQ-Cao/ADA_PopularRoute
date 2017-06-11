b75fa57daf4a059a2ae32c6ab54de8f779e43c15
caojiaqing <caojiaqing1991@gmail.com>
2017-06-11 16:59:48 +0800

    常量值类

---
+++ src/main/java/model/Constant.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created by caojiaqing on 11/06/2017.
+ */
+public class Constant {
+    // 左转cost，单位米，double
+    public static double leftHandCost = 50.0;
+    // 左转判断角度差，单位度，double型
+    public static double leftHandAzimuthDis = 20.0;
+}
