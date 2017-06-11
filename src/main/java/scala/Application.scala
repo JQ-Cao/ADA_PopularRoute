cb360c77c2d90183a2ff997fcbe77fb9d00c980a
caojiaqing <caojiaqing1991@gmail.com>
2017-06-11 17:00:50 +0800

    增加了基本逻辑处理

--- src/main/java/scala/Application.scala
+++ src/main/java/scala/Application.scala
@@ -1,8 +1,30 @@
 package scala
 
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.trajectory.{CennaviRoadnetwork, CennaviTrajectory}
+
 /**
   * Created by caojiaqing on 23/05/2017.
   */
 object Application {
 
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+    conf.setAppName("ADA trajectory")
+    conf.setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+
+    val roadnetworkRDD = CennaviRoadnetwork.loadRoadnetworkFromCSV(sc.textFile("/Users/caojiaqing/Repository/map/beijing/R-G.csv"))
+
+    val trajectoryRDD = CennaviTrajectory.toTrajectory(sc.textFile("/Users/caojiaqing/Repository/Trajectory/23")).collect()
+
+
+    print("")
+
+    sc.stop()
+  }
 }
